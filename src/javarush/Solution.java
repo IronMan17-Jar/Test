c9543faa42d2391abe7206d35609c818a18b635e
IronMan17-Jar <danil1704@mail.ru>
2021-01-16 10:21:46 +0300

    ww

--- src/javarush/Solution.java
+++ src/javarush/Solution.java
@@ -4,38 +4,24 @@ import java.util.Arrays;
 
 public class Solution{
 
-    public static void main(String[] args) {
+    public static void main (String[] args) throws  Exception {
 
 
         int[] array = new int[20];
+        int min= array[0];
+        int max= array[0];
         for (int x = 0; x < array.length; x++) {
             array[x] = (int) (Math.random() * 30);
-            System.out.println(array[x]);
+            if(array[x]<min)
+              min=array[x];
+            if(array[x]>max)
+            max=array[x];
         }
-
+        System.out.println(min);
         System.out.println();
-
-        for (int x = array.length-1; x > 1; x--) {
-            for (int v = 0; v < x; v++) {
-                if (array[v] > array[v + 1]) {
-                    int max = array[v];
-                    array[v] = array[v + 1];
-                    array[v + 1] = max;
-
-                }
-            }
-        }
-
-        System.out.println(array[0]);
-        System.out.println();
-        System.out.println(array[19]);
-
+        System.out.println(max);
     }
-
 }
-
-
-
 
 
 
