15853f8e7d421212d91438472f3feef9cbe594da
IronMan17-Jar <danil1704@mail.ru>
2021-02-22 20:43:32 +0300

    Lesson2222

--- Project3/src/OperationsWithNumbers.java
+++ Project3/src/OperationsWithNumbers.java
@@ -20,15 +20,16 @@ public class OperationsWithNumbers {
 
     }
         private int numberProcessing(char symbol){
-            if(symbol == 'I'){ return 1; }
-            else if(symbol == 'V'){return 5; }
-            else if(symbol == 'X'){ return 10; }
-            else if(symbol == 'L'){ return 50; }
-            else if(symbol == 'C'){ return 100; }
-            else if(symbol == 'D'){ return 500; }
-            else if(symbol == 'M'){ return 1000; }
-            else { return -1; }
-
+                switch (symbol) {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return -1;
+            }
         }
         public String arabToRoman(int number){
             String romanVal = "";
