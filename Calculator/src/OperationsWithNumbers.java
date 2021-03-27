3969cbe426fe37bb769d1c9c1aa2649994e23b1a
IronMan17-Jar <danil1704@mail.ru>
2021-03-27 19:08:07 +0300

    Calcula

--- Calculator/src/OperationsWithNumbers.java
+++ Calculator/src/OperationsWithNumbers.java
@@ -1,43 +1,107 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class OperationsWithNumbers {
     public static int[] arab = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     public static String[] roman = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
-
     public static void outputOnDisplay() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите данные:");
         String[] cell = scanner.nextLine().split(" ");
-        int number1 = 0, number2 = 0;
-        int isRoman1 = 0, isRoman2 = 0;
-        boolean flag = false;
+        int first = 0;
+        int second = 0;
         String operation = " ";
         operation = cell[1];
         char operation2 = operation.charAt(0);
-        for (int x = 0; x < roman.length; x++) {
-            if (roman[x].equals(cell[0]) && roman[x].equals(cell[2])) {
-                flag = true;
-            }
+        if (checkData(cell)) {
             try {
-                if (flag) {
-                    number1 = ConversionOfNumbers.romanToArab(cell[0]);
-                    number2 = ConversionOfNumbers.romanToArab(cell[2]);
-                    System.out.println(ConversionOfNumbers.arabToRoman(Arithmetic.arithmetiOperations(number1, number2, operation2)));
-                } else {
-                    number1 = Integer.parseInt(cell[0]);
-                    number2 = Integer.parseInt(cell[2]);
-                    System.out.println(Arithmetic.arithmetiOperations(number1, number2, operation2));
-                }
-            } catch (IllegalArgumentException e) {
-                if ((number1 > 10 || number1 < 1) || (number2 > 10 || number2 < 1)) {
-                    throw new IllegalArgumentException();
-                }
-            }
+                    first = Integer.parseInt(cell[0]);
+                    second = Integer.parseInt(cell[2]);
+                    System.out.println(Arithmetic.arithmetiOperations(first, second, operation2));
 
+            } catch (IllegalArgumentException e) {
+                first = ConversionOfNumbers.romanToArab(cell[0]);
+                second = ConversionOfNumbers.romanToArab(cell[2]);
+                System.out.println(ConversionOfNumbers.arabToRoman(Arithmetic.arithmetiOperations(first, second, operation2)));
+            }
         }
 
     }
+    private static boolean checkData(String[] cell) {
+        if (cell.length != 3) {
+            return false;
+        }
+        checkOperation(cell);
+        checkFirstAndSecond(cell);
+        checkNumbers(cell);
+        return true | false;
+    }
 
+    /*private static boolean checkFirst(String[] cell){
+        String first = cell[0];
+        String second = cell[2];
+        if(cell[0].equals(roman) && cell[2].equals(roman)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private static boolean checkSecond(String[] cell){
+        String first = cell[0];
+        String second = cell[2];
+        if(cell[0].equals(arab) && cell[2].equals(arab)){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }*/
+
+    private static boolean checkFirstAndSecond(String[] cell){
+
+        if (Arrays.asList(cell[0]).contains(arab) && Arrays.asList(cell[2]).contains(arab)) {
+            return true;
+        }
+        else if (Arrays.asList(cell[0]).contains(roman) && Arrays.asList(cell[2]).contains(roman)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private static boolean checkOperation(String[] cell){
+        String operation = cell[1];
+        if(operation != "+" || operation != "-" || operation != "*" || operation != "/"){
+            return false;
+        }
+        return true;
+    }
+    private static boolean checkNumbers(String[] cell){
+
+        for(String x : cell) {
+            try {
+                int first = ConversionOfNumbers.romanToArab(cell[0]);
+                int second = ConversionOfNumbers.romanToArab(cell[2]);
+                if ((first <= 10 || first >= 1) && (second <= 10 || second >= 1)) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+            catch (IllegalArgumentException e){
+                int first = Integer.parseInt(cell[0]);
+                int second = Integer.parseInt(cell[2]);
+                if ((first <= 10 && first >= 1) && (second <= 10 && second >= 1)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+
+    }
 }
-
